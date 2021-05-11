7f443d0f45fd71034a6bfdf94b696f5fc29163ad
sohaibElashab <62187883+sohaibElashab@users.noreply.github.com>
2021-05-11 20:26:41 +0000

    add pages

---
+++ resources/js/components/Pages/notification.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <!-- Wrapper Start -->
+        <div class="wrapper">
+            <!-- Sidebar  -->
+            <sidebar />
+            <!-- Sidebar End  -->
+
+            <!-- TOP Nav Bar -->
+            <topbar />
+            <!-- TOP Nav Bar END -->
+
+            <!-- Right Sidebar Panel Start-->
+            <rightbar />
+            <!-- Right Sidebar Panel End-->
+
+            <!-- Page Content  -->
+            <contentHome />
+            <!-- Page Content End -->
+        </div>
+        <!-- Wrapper END -->
+        <bottombar />
+        <!-- Footer -->
+    </div>
+</template>
+
+<script>
+import contentHome from "./../Sections/general_section/dynamic_section/contentHome.vue";
+export default {
+    components: {
+        contentHome
+    }
+};
+</script>
